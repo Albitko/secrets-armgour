3e818f11214f4037380ea8ca415c26dc6fe521ef
Alex Bitkov <alexey.b@locus.finance>
2023-08-02 17:02:32 +0300

    use case for credentials sending

--- internal/secrets/sender/sender.go
+++ internal/secrets/sender/sender.go
@@ -1,9 +1,21 @@
 package sender
 
+import (
+	"fmt"
+)
+
 type httpAPI interface {
+	SendCredentials(serviceName, serviceLogin, servicePassword, meta string) error
 }
+
 type sender struct {
 	api httpAPI
+}
+
+func (s *sender) CreateCredentials(serviceName, serviceLogin, servicePassword, meta string) error {
+	fmt.Println(serviceName, serviceLogin, servicePassword, meta)
+	err := s.api.SendCredentials(serviceName, serviceLogin, servicePassword, meta)
+	return err
 }
 
 func New(api httpAPI) *sender {
