1fb551f265a54f4e485a656f2513032199e5d566
Alex Bitkov <alexey.b@locus.finance>
2023-08-02 17:04:12 +0300

    client for credentials sending

--- internal/adapter/httpapi/armgour.go
+++ internal/adapter/httpapi/armgour.go
@@ -1,8 +1,34 @@
 package httpapi
 
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+
+	"github.com/Albitko/secrets-armgour/internal/entity"
+)
+
 type httpAPI struct {
+	armgourURL string
+	client     *resty.Client
 }
 
-func New() *httpAPI {
-	return &httpAPI{}
+func (a *httpAPI) SendCredentials(serviceName, serviceLogin, servicePassword, meta string) error {
+	userCredentials := entity.UserCredentials{
+		ServiceLogin:    serviceLogin,
+		ServiceName:     serviceName,
+		ServicePassword: servicePassword,
+		Meta:            meta,
+	}
+	resp, _ := a.client.R().SetBody(userCredentials).Post(a.armgourURL + "/v1/secrets/credentials/create")
+	fmt.Println(resp.String())
+	return nil
+}
+
+func New(serverURL string) *httpAPI {
+	r := resty.New()
+	return &httpAPI{
+		armgourURL: serverURL,
+		client:     r,
+	}
 }
