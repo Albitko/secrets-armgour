54d51f43e51a604b7442697e9658a80fc81152a0
Alex Bitkov <alexey.b@locus.finance>
2023-08-02 19:27:48 +0300

    creation processsor

--- internal/secrets/processor/processor.go
+++ internal/secrets/processor/processor.go
@@ -1,10 +1,46 @@
 package processor
 
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/Albitko/secrets-armgour/internal/entity"
+)
+
 type repository interface {
+	InsertCard(card entity.UserCard) error
+	InsertCredentials(credentials entity.UserCredentials) error
+	InsertBinary(bin entity.UserBinary, data []byte) error
+	InsertText(text entity.UserText) error
 }
 
 type processor struct {
 	repo repository
+}
+
+func (p processor) BinaryCreation(binary entity.UserBinary) error {
+	decodedContent, err := base64.StdEncoding.DecodeString(binary.B64Content)
+	if err != nil {
+		fmt.Println("Error decoding content:", err)
+		return err
+	}
+	err = p.repo.InsertBinary(binary, decodedContent)
+	return err
+}
+
+func (p processor) TextCreation(text entity.UserText) error {
+	err := p.repo.InsertText(text)
+	return err
+}
+
+func (p processor) CredentialsCreation(credentials entity.UserCredentials) error {
+	err := p.repo.InsertCredentials(credentials)
+	return err
+}
+
+func (p processor) CardCreation(card entity.UserCard) error {
+	err := p.repo.InsertCard(card)
+	return err
 }
 
 func New(repo repository) *processor {
