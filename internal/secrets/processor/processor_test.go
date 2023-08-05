ef07cd50f00bd391f452fbe20d9f8a1591b737bf
Alex Bitkov <alexey.b@locus.finance>
2023-08-05 21:19:43 +0300

    use case tests

--- internal/secrets/processor/processor_test.go
+++ internal/secrets/processor/processor_test.go
@@ -47,3 +47,93 @@ func Test_Login(t *testing.T) {
 		})
 	}
 }
+
+func Test_CardEdit(t *testing.T) {
+	authTests := []struct {
+		name        string
+		index       string
+		intIndex    int
+		card        entity.UserCard
+		expectedErr interface{}
+	}{
+		{
+			name:        "CardEdit: Succes",
+			index:       "1234",
+			intIndex:    1234,
+			card:        entity.UserCard{},
+			expectedErr: nil,
+		},
+	}
+	for _, tt := range authTests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			mockRepo := newMockRepository(t)
+			mockRepo.EXPECT().UpdateCard(ctx, tt.intIndex, tt.card).Return(nil).Once()
+			secretsProcessor := New(mockRepo)
+			err := secretsProcessor.CardEdit(ctx, tt.index, tt.card)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+func Test_BinaryEdit(t *testing.T) {
+	authTests := []struct {
+		name        string
+		index       string
+		intIndex    int
+		b64data     []byte
+		binary      entity.UserBinary
+		expectedErr interface{}
+	}{
+		{
+			name:     "BinaryEdit: Succes",
+			index:    "1234",
+			intIndex: 1234,
+			b64data:  []byte("test string"),
+			binary: entity.UserBinary{
+				B64Content: "dGVzdCBzdHJpbmc=",
+			},
+			expectedErr: nil,
+		},
+	}
+	for _, tt := range authTests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			mockRepo := newMockRepository(t)
+			mockRepo.EXPECT().UpdateBinary(ctx, tt.intIndex, tt.binary, tt.b64data).Return(nil).Once()
+			secretsProcessor := New(mockRepo)
+			err := secretsProcessor.BinaryEdit(ctx, tt.index, tt.binary)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+func Test_TextEdit(t *testing.T) {
+	authTests := []struct {
+		name        string
+		index       string
+		intIndex    int
+		text        string
+		textObj     entity.UserText
+		expectedErr error
+	}{
+		{
+			name:     "TextEdit: Succes",
+			index:    "1234",
+			intIndex: 1234,
+			text:     "string",
+			textObj: entity.UserText{},
+			expectedErr: nil,
+		},
+	}
+	for _, tt := range authTests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			mockRepo := newMockRepository(t)
+			mockRepo.EXPECT().UpdateText(ctx, tt.intIndex, tt.textObj).Return(nil).Once()
+			secretsProcessor := New(mockRepo)
+			err := secretsProcessor.TextEdit(ctx, tt.index, tt.textObj)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
