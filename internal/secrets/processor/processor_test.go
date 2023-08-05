41c4d08bc05a24a88ce5f2883d6c8bcc7e5d6c0d
Alex Bitkov <alexey.b@locus.finance>
2023-08-05 18:22:59 +0300

    test login

--- internal/secrets/processor/processor_test.go
+++ internal/secrets/processor/processor_test.go
@@ -4,23 +4,46 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/Albitko/secrets-armgour/internal/entity"
 )
 
 func Test_Login(t *testing.T) {
-	ctx := context.Background()
-	testCredentials := entity.UserAuth{
-		Login:    "test_login",
-		Password: "test_password",
+	authTests := []struct {
+		name        string
+		login       string
+		password    string
+		hashedPass  string
+		expectedErr error
+	}{
+		{
+			name:        "Auth: success",
+			login:       "user",
+			password:    "secretpass",
+			hashedPass:  "$2a$10$D7P7FfZnPw6UKJfHofAWEuuFmEacoHpl/T9LH3zFCOlMJhaVTpxGy",
+			expectedErr: nil,
+		},
+		{
+			name:        "Auth: fail",
+			login:       "user",
+			password:    "secretpass",
+			hashedPass:  "$2a$10$D7P7FfZnPw6UKJfHofAWGuuFmEacoHpl/T9LH3zFCOlMJhaVTpxGy",
+			expectedErr: entity.ErrInvalidCredentials,
+		},
 	}
-
-	mockRepo := newMockRepository(t)
-	mockRepo.EXPECT().GetUserPasswordHash(ctx, testCredentials.Login).Return("0x0", nil).Once()
-
-	secretsProcessor := New(mockRepo)
-
-	err := secretsProcessor.LoginUser(ctx, testCredentials)
-	if err != nil {
-		return
+	for _, tt := range authTests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			testCredentials := entity.UserAuth{
+				Login:    tt.login,
+				Password: tt.password,
+			}
+			mockRepo := newMockRepository(t)
+			mockRepo.EXPECT().GetUserPasswordHash(ctx, tt.login).Return(tt.hashedPass, nil).Once()
+			secretsProcessor := New(mockRepo)
+			err := secretsProcessor.LoginUser(ctx, testCredentials)
+			assert.Equal(t, tt.expectedErr, err)
+		})
 	}
 }
