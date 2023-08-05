7fdcae7d1f0e3e66983e78d8d7989981da140004
Alex Bitkov <alexey.b@locus.finance>
2023-08-05 13:45:37 +0300

    build variables

--- internal/app/client/app.go
+++ internal/app/client/app.go
@@ -12,7 +12,13 @@ import (
 
 func Run() {
 	serviceURL := flag.String("address", "http://localhost:8080", "Address of armGour service")
+	version := flag.String("version", "0.1.0", "Version of armGour service")
+	buildAt := flag.String("date", "05.08.2023", "Date of build")
 	flag.Parse()
+	fmt.Println("Secrets armGour version", *version)
+	fmt.Println("Build at", *buildAt)
+	fmt.Println("===================================")
+
 	serviceAPI := httpapi.New(*serviceURL)
 	secretsSender := sender.New(serviceAPI)
 	c := cli.New(secretsSender)
