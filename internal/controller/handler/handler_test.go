31fcb1af201a8859499b36f5fe8c0c455612df5a
Alex Bitkov <alexey.b@locus.finance>
2023-08-06 10:22:21 +0300

    test register handler

--- internal/controller/handler/handler_test.go
+++ internal/controller/handler/handler_test.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -53,4 +54,63 @@ func TestLogin_InvalidCredentials(t *testing.T) {
 	h.Login(ctx)
 	assert.Equal(t, http.StatusUnauthorized, w.Code)
 	mockProcessor.AssertCalled(t, "LoginUser", mock.Anything, auth)
+}
+
+func TestRegister_Success(t *testing.T) {
+	mockProcessor := newMockSecretsProcessor(t)
+	h := &handler{
+		processor: mockProcessor,
+	}
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	auth := entity.UserAuth{
+		Login:    "user",
+		Password: "password",
+	}
+	body, _ := json.Marshal(auth)
+	ctx.Request, _ = http.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(body))
+
+	mockProcessor.On("RegisterUser", mock.Anything, auth).Return(nil)
+	h.Register(ctx)
+	assert.Equal(t, http.StatusOK, w.Code)
+	mockProcessor.AssertCalled(t, "RegisterUser", mock.Anything, auth)
+}
+
+func TestRegister_LoginAlreadyInUse(t *testing.T) {
+	mockProcessor := newMockSecretsProcessor(t)
+	h := &handler{
+		processor: mockProcessor,
+	}
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	auth := entity.UserAuth{
+		Login:    "user",
+		Password: "password",
+	}
+	body, _ := json.Marshal(auth)
+	ctx.Request, _ = http.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(body))
+	mockProcessor.On("RegisterUser", mock.Anything, auth).Return(entity.ErrLoginAlreadyInUse)
+	h.Register(ctx)
+	assert.Equal(t, http.StatusConflict, w.Code)
+	mockProcessor.AssertCalled(t, "RegisterUser", mock.Anything, auth)
+}
+
+func TestRegister_InternalServerError(t *testing.T) {
+	mockProcessor := newMockSecretsProcessor(t)
+	h := &handler{
+		processor: mockProcessor,
+	}
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	auth := entity.UserAuth{
+		Login:    "user",
+		Password: "password",
+	}
+
+	body, _ := json.Marshal(auth)
+	ctx.Request, _ = http.NewRequest(http.MethodPost, "/v1/user/register", bytes.NewReader(body))
+	mockProcessor.On("RegisterUser", mock.Anything, auth).Return(errors.New("some error"))
+	h.Register(ctx)
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+	mockProcessor.AssertCalled(t, "RegisterUser", mock.Anything, auth)
 }
